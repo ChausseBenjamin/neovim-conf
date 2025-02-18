9af26b4ec30ee1f10a0c884e7ac64e235ddd8bff
Benjamin Chausse <benjamin.chausse@goto.com>
2025-02-18 18:48:20 -0500

    One less plugins for color highlighting

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,9 +1,41 @@
 return {
-  { -- Easy color conversions (rgb, hex, hsl, ...)
-    "amadeus/vim-convert-color-to"
-  },
-  { -- Highlight hex/rgb colors inside code
-    "norcalli/nvim-colorizer.lua",
-    config = true,
-  },
+	{ -- Easy color conversions (rgb, hex, hsl, ...)
+		"amadeus/vim-convert-color-to",
+		keys = {
+			{
+				"<leader>ox",
+				"<cmd>ConvertColorTo hex<cr>",
+				mode = { "n", "x", "o" },
+				desc = "C[o]nvert to He[x] (ex: #001122)",
+			},
+			{
+				"<leader>oc",
+				"<cmd>ConvertColorTo hex_num<cr>",
+				mode = { "n", "x", "o" },
+				desc = "C[o]nvert to [C] style int (ex: 0x001122)",
+			},
+			{
+				"<leader>or",
+				"<cmd>ConvertColorTo rgb<cr>",
+				mode = { "n", "x", "o" },
+				desc = "C[o]nvert to [R]gb (ex: rgb(0, 100, 200))",
+			},
+			{
+				"<leader>of",
+				"<cmd>ConvertColorTo rgb_float<cr>",
+				mode = { "n", "x", "o" },
+				desc = "C[o]nvert to [F]loat RGB (ex: rgba(0.1, 0.2, 0.3))",
+			},
+			{
+				"<leader>oh",
+				"<cmd>ConvertColorTo hsl<cr>",
+				mode = { "n", "x", "o" },
+				desc = "C[o]nvert to [H]sl (ex: hsl(200, 20%, 10%))",
+			},
+		},
+	},
+	-- { -- Highlight hex/rgb colors inside code
+	-- 	"norcalli/nvim-colorizer.lua",
+	-- 	config = true,
+	-- },
 }
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -19,6 +19,11 @@ return {
 			require("mini." .. pkg).setup()
 		end
 
+		-- Safeguard while I get used to mini.surround...
+		vim.keymap.set("n", "cs", function()
+			vim.notify("Oops, your muscle memory tried using the Tpope surround...")
+		end)
+
 		-- Animations and delay were annoying for this one...
 		local no_indent_anim = require("mini.indentscope").gen_animation.none
 		require("mini.indentscope").setup({
@@ -83,6 +88,108 @@ return {
 
 			-- `c` already used by treesitter-textobjects
 			comment = { suffix = "g", options = {} },
+			quickfix = { suffix = "q", options = { wrap = false } },
+		})
+
+		local function hsl_to_rgb(h, s, l)
+			s = math.max(0, math.min(1, s))
+			l = math.max(0, math.min(1, l))
+
+			local function hue_to_rgb(p, q, t)
+				if t < 0 then
+					t = t + 1
+				end
+				if t > 1 then
+					t = t - 1
+				end
+				if t < 1 / 6 then
+					return p + (q - p) * 6 * t
+				end
+				if t < 1 / 2 then
+					return q
+				end
+				if t < 2 / 3 then
+					return p + (q - p) * (2 / 3 - t) * 6
+				end
+				return p
+			end
+
+			h = (h % 360) / 360
+			local q = l < 0.5 and (l * (1 + s)) or (l + s - l * s)
+			local p = 2 * l - q
+
+			local r = hue_to_rgb(p, q, h + 1 / 3)
+			local g = hue_to_rgb(p, q, h)
+			local b = hue_to_rgb(p, q, h - 1 / 3)
+
+			return math.floor(r * 255), math.floor(g * 255), math.floor(b * 255)
+		end
+
+		local hex_colorgroup = require("mini.hipatterns").compute_hex_color_group
+		local gen_highlighter = require("mini.hipatterns").gen_highlighter
+		require("mini.hipatterns").setup({
+			delay = {
+				text_change = 80,
+				scroll = 25,
+			},
+			highlighters = {
+				rgba = { -- Ex: rgba(103, 199, 163, 0.4)
+					pattern = "rgba%(%d+,%s*%d+,%s*%d+,%s*[01]?%.?%d*%)",
+					group = function(_, match)
+						local r, g, b = match:match("rgba%((%d+),%s*(%d+),%s*(%d+),%s*[01]?%.?%d*%)")
+						local hex = string.format("#%02x%02x%02x", r, g, b) -- Ignore alpha
+						return require("mini.hipatterns").compute_hex_color_group(hex, "bg")
+					end,
+				},
+				hexa = { -- Ex: #C0FFEE42
+					pattern = "#%x%x%x%x%x%x%x%x%f[%X]",
+					group = function(_, match)
+						local hex = match:sub(1, 7) -- Ignore the last two alpha characters
+						return require("mini.hipatterns").compute_hex_color_group(hex, "bg")
+					end,
+				},
+				short_hex_color = { -- Ex: #bad
+					pattern = "#%x%x%x%f[%X]",
+					group = function(_, match)
+						local r, g, b = match:sub(2, 2), match:sub(3, 3), match:sub(4, 4)
+						local hex = string.format("#%s%s%s%s%s%s", r, r, g, g, b, b)
+						return hex_colorgroup(hex, "bg")
+					end,
+				},
+				hex_color = gen_highlighter.hex_color(),
+				c_hex_long = { -- Ex: 0xb7416e
+					pattern = "0x%x%x%x%x%x%x%f[%X]",
+					group = function(_, match)
+						local hex = "#" .. match:sub(3)
+						return hex_colorgroup(hex, "bg")
+					end,
+				},
+				c_hex_short = { -- Ex: 0xfa5
+					pattern = "0x%x%x%x%f[%X]",
+					group = function(_, match)
+						local r, g, b = match:sub(3, 3), match:sub(4, 4), match:sub(5, 5)
+						local hex = string.format("#%s%s%s%s%s%s", r, r, g, g, b, b)
+						return hex_colorgroup(hex, "bg")
+					end,
+				},
+				rgb = { -- Ex: rgb(000,111,222)
+					pattern = "rgb%(%d+,%s*%d+,%s*%d+%)",
+					group = function(_, match)
+						local r, g, b = match:match("rgb%((%d+),%s*(%d+),%s*(%d+)%)")
+						local hex = string.format("#%02x%02x%02x", r, g, b)
+						return hex_colorgroup(hex, "bg")
+					end,
+				},
+				hsl = { -- Ex: hsl(180,54%,45%)
+					pattern = "hsl%(%d+,%s*%d+%%,%s*%d+%%%)",
+					group = function(_, match)
+						local h, s, l = match:match("hsl%((%d+),%s*(%d+)%%,%s*(%d+)%%%)")
+						local r, g, b = hsl_to_rgb(tonumber(h), tonumber(s) / 100, tonumber(l) / 100)
+						local hex = string.format("#%02x%02x%02x", r, g, b)
+						return hex_colorgroup(hex, "bg")
+					end,
+				},
+			},
 		})
 	end,
 }
