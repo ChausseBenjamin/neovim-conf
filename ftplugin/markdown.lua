46fe569b07f65941385db56f2a6507b8a0917770
Benjamin Chausse <benjamin@chausse.xyz>
2025-10-02 12:40:39 -0400

    images with kitty (this is scarily close to emacs)

---
+++ ftplugin/markdown.lua
@@ -0,0 +1,13 @@
+--                       _       _
+--  _ __ ___   __ _ _ __| | ____| | _____      ___ __
+-- | '_ ` _ \ / _` | '__| |/ / _` |/ _ \ \ /\ / / '_ \
+-- | | | | | | (_| | |  |   < (_| | (_) \ V  V /| | | |
+-- |_| |_| |_|\__,_|_|  |_|\_\__,_|\___/ \_/\_/ |_| |_|
+--
+-- The best document format for note-taking/wikis
+
+vim.pack.add({
+	{ src = GH .. 'iamcco/markdown-preview.nvim' }
+})
+
+require('plugins.images')
